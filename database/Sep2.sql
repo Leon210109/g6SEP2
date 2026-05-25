b435889bdc149adc70f58759105922de22cc0733
Leon210109 <144994653+Leon210109@users.noreply.github.com>
2026-05-25 23:47:49 +0200

    save and updated sql

--- database/Sep2.sql
+++ database/Sep2.sql
@@ -169,8 +169,9 @@ INSERT INTO listing
      last_Renovated, max_number_of_people, country, region, street, room_number, floor,
      postal_code, check_in_time, check_out_time, listing_type)
 VALUES
-    (1, 3, 2, TRUE, 95, 6800, '2022-08-15', 4, 'Denmark', 'Copenhagen', 'Niels Juels Gade', '12A', 3, '1054', '15:00:00', '11:00:00', 'SHORT_TERM'),
-    (2, 2, 1, TRUE, 71, 6295, '2025-04-03', 2, 'Denmark', 'Horsens',    'Sundvej',          '2',   3, '8700', '15:00:00', '11:00:00', 'LONG_TERM');
+    (1, 3, 2, TRUE,  95,  6800, '2022-08-15', 4, 'Denmark', 'Copenhagen', 'Niels Juels Gade', '12A', 3, '1054', '15:00:00', '11:00:00', 'SHORT_TERM'),
+    (2, 2, 1, TRUE,  71,  6295, '2025-04-03', 2, 'Denmark', 'Horsens',   'Sundvej',           '2',  3, '8700', '15:00:00', '11:00:00', 'LONG_TERM'),
+    (3, 4, 2, FALSE, 110, 7500, '2023-06-20', 5, 'Denmark', 'Copenhagen', 'Ameliegade',       '5B', 2, '1000', '14:00:00', '10:00:00', 'SHORT_TERM');
 
 -- Bookings (listing 1 is SHORT_TERM; listing 2 is LONG_TERM and uses tenancy_application)
 INSERT INTO Booking
